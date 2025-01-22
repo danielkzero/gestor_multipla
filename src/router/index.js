e2b13f315e088c5af225a4d8025cfa10bbfed0eb
Daniel <danikzero@hotmail.com>
2025-01-22 16:27:17 -0300

    Adicinando authGuard, caso a verificação de status de token falar manda o usuário devoltar ao painel de login

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from "../stores/auth";
+
+export const authGuard = async (to, from, next) => {
+  const authStore = useAuthStore();
+  const isAuthenticated = await authStore.verifyStatus();
+  if (!isAuthenticated) {
+    next({ name: "login" });
+  } else {
+    next();
+  }
+};
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,33 +24,37 @@ const router = createRouter({
       name: 'Inicio',
       redirect: '/principal',
       component: () => import('../views/layout/principal.vue'),
+      beforeEnter: authGuard,
       children: [
         {
           path: '/principal',
           name: 'Principal',
-          component: () => import('../views/paginas/principal.vue')
+          component: () => import('../views/paginas/principal.vue'),
+          beforeEnter: authGuard,
         },
         {
           path: '/pedido/:id',
           name: 'PedidoDetalhes',
-          component: () => import('../views/paginas/pedido_detalhes.vue')
+          component: () => import('../views/paginas/pedido_detalhes.vue'),
+          beforeEnter: authGuard,
         },
         {
           path: '/usuarios',
           name: 'Usuarios',
-          component: () => import('../views/paginas/lista_usuarios.vue')
+          component: () => import('../views/paginas/lista_usuarios.vue'),
+          beforeEnter: authGuard,
         },
         {
           path: "/cadastro/usuarios",
           name: "CadastroUsuario",
-          component: () =>
-            import("../views/paginas/cadastro_usuario.vue"),
+          component: () => import("../views/paginas/cadastro_usuario.vue"),
+          beforeEnter: authGuard,
         },
         {
           path: "/cadastro/usuarios/:id",
           name: "CadastroUsuarioId",
-          component: () =>
-            import("../views/paginas/cadastro_usuario.vue"),
+          component: () => import("../views/paginas/cadastro_usuario.vue"),
+          beforeEnter: authGuard,
         },
       ],
     },
